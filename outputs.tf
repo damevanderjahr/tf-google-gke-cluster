fc5c0a72f78d38a3c2d89da1f598d9f617eae0a9
Dmytro Zharynin <dmzh@solvve.com>
2024-05-06 16:42:01 +0300

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,20 @@ output "kubeconfig" {
   value       = "${path.module}/kubeconfig"
   description = "The path to the kubeconfig file"
 }
+
+output "endpoint" {
+  value       = module.gke_auth.endpoint
+  description = "Cluster endpoint"
+}
+
+output "config_ca" {
+  sensitive   = true
+  value       = module.gke_auth.ca_certificate
+  description = "Cluster ca certificate (base64 encoded)"
+}
+
+output "token" {
+  sensitive   = true
+  value       = module.gke_auth.token
+  description = "The token value for use with the kubernetes provider."
+}
